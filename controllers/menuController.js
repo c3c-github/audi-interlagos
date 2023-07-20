919486a00424425d6aa7027d6d38b47e3dc89b46
Tulio Sales <tulio.sales@c3csoftware.com.br>
2023-07-20 16:23:22 -0300

    v0.1

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,22 +1,7 @@
 const jsforce = require('jsforce');
 
-const menu = (req, res) => {
-    res.render("menu", {} );
-}
-const formPay = (req, res) => {
+const form = (req, res) => {
     res.render("form", {form : {'name': 'PAYMENTS'}} );
-}
-const formVentures = (req, res) => {
-    res.render("form", {form : {'name': 'VENTURES'}} );
-}
-const formPrime = (req, res) => {
-    res.render("form", {form : {'name': 'PRIME'}} );
-}
-const formCheckOut = (req, res) => {
-    res.render("form", {form : {'name': 'CHECKOUT'}} );
-}
-const formApp = (req, res) => {
-    res.render("form", {form : {'name': 'APP'}} );
 }
 
 const thanks = (req, res) => {
@@ -116,11 +101,6 @@ const thanks = (req, res) => {
 }
 
 module.exports =  {
-    menu,
-    formPay,
-    formVentures,
-    formPrime,
-    formCheckOut,
-    formApp,
+    form,
     thanks
 };
--- routes/menu.js
+++ routes/menu.js
@@ -1,13 +1,7 @@
 const express = require('express');
-const {menu, formPay, formVentures, formPrime, formCheckOut, formApp, thanks } = require('../controllers/menuController');
+const {form, thanks } = require('../controllers/menuController');
 const router = express.Router();
-router.get('/', menu);
-router.get('/formPay', formPay);
-router.get('/formVentures', formVentures);
-router.get('/formPrime', formPrime);
-router.get('/formCheckOut', formCheckOut);
-router.get('/formApp', formApp);
-
+router.get('/', form);
 router.post('/submit-form', thanks);
 
 module.exports = router;
