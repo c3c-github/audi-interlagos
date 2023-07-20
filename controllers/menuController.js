038f7efaf32db0ccadf9fb0047900a9a31fed20a
Tulio Sales <tulio.sales@c3csoftware.com.br>
2023-07-20 17:36:34 -0300

    v0.2

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,103 +1,98 @@
-const jsforce = require('jsforce');
+const http = require('https');
 
 const form = (req, res) => {
     res.render("form", {form : {'name': 'PAYMENTS'}} );
 }
 
 const thanks = (req, res) => {
-        // replace with your Salesforce Connected App credentials
-        const clientId = '3MVG9cHH2bfKACZa7IhT4bhgY3L.B7OfXN5.2Q8eEu_jK7ebe1SeIwayHY1AdDQt0rK7VYwgVMivS2ge1W86T';
-        const clientSecret = '325E76F1AE9726A62C8FAA69A0F7281078E5DDC5EA437053331913175F15EFF7';
-        const username = 'mauro.araripe@c3csoftware.com.br';
-        const password = 'c3c*2023';
-        const securityToken = 'czFFo1un2JJxVP88If9Zqh16';
-    
-        const conn = new jsforce.Connection({
-            oauth2: {
-                clientId: clientId,
-                clientSecret: clientSecret,
-                redirectUri: 'http://localhost:3000/oauth2/callback' // replace with your callback URL
-            }
+        const postData = JSON.stringify({
+            grant_type: "client_credentials",
+            client_id: "bbov8owdgojqammn59ejtsbq",
+            client_secret: "kYC6uRZeNDIdMZAPKWqC5HXD"    
         });
 
-        conn.login(username, password + securityToken, function(err, userInfo) {
-            if (err) { return console.error(err); }
-            console.log('Logged in to Salesforce');
-            console.log('Access Token: ' + conn.accessToken);
-            console.log('Instance URL: ' + conn.instanceUrl);
-            console.log('body: ',req.body);
-            console.log('body.product: ',req.body.product);
-            console.log('body.product.typeof: ', typeof (req.body.product) )
-
-            var product = '';
-
-            if(req.body.product != undefined){
-                if(typeof req.body.product == 'string'){
-                    product = req.body.product;
-                }
-                else{
-                    for(var i =0 ; i < req.body.product.length; i++){
-                        product = product + req.body.product[i] + ';'
-                    }
-                }
-                console.log('product: ',product);
+        const options = {
+            hostname: 'mcc62qyxnq0y3c33dwzj4mkhzw0q.auth.marketingcloudapis.com',
+            path: '/v2/token',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': postData.length,
             }
+        };
 
-            var recordTypeId = '';
+        const authReq = http.request(options, (resAuth) => {
+            let responseData = '';
 
-            switch (req.body.formType) {
-                case 'PAYMENTS':
-                    recordTypeId = '012HY0000004La3YAE';
-                    break
-                case 'VENTURES':
-                    recordTypeId = '012HY0000004La8YAE';
-                    break
-                case 'PRIME':
-                    recordTypeId = '012HY0000004LaDYAU';
-                    break
-                case 'CHECKOUT':
-                    recordTypeId = '012HY0000004LaIYAU';
-                    break
-                case 'APP':
-                    recordTypeId = '012HY0000004LaNYAU';
-                    break
-                default:
-                    recordTypeId = '012HY0000004LXiYAM';
-                    break
-              }
-            
-
-            const lead = {
-                RecordTypeId : recordTypeId,
-                LastName : req.body.name,
-                Email : req.body.email,
-                MobilePhone : req.body.phone,
-                Company : req.body.company,
-                ProdutosDeinteresse__c : product,
-                Description : req.body.message,
-                JaInvesteCripto__c : req.body.cripto,
-                ColaboradorTransfero__c : req.body.ownerName,
-                OrigemWebSummit__c : req.body.formType,
-                Advisor__c : req.body.advisor
-            };  
-    
-            conn.sobject('Lead').create(lead, function(err, result) {
-            if (err) {return console.error(err); }
-                console.log('Lead inserted with Id: ' + result.id);                
+            resAuth.on('data', (chunk) => {
+                responseData += chunk;
             });
-            
-            res.render("thanks", {form : {'name': req.body.formType}} );
 
+            resAuth.on('end', () => {
+                
+                let authResponse = JSON.parse(responseData);
+
+                const postDE = JSON.stringify([
+                    {
+                        keys:{
+                            "Id": req.body.document
+                        },
+                        values:{
+                            "Nome": req.body.name,
+                            "Telefone" : req.body.phone,
+                            "Email" : req.body.email,
+                            "Estado" : req.body.state,
+                            "Cidade" : req.body.city,
+                            "Documento" : req.body.document,
+                            "Concessionaria" : req.body.seller,
+                            "Modelo" : req.body.vehiclemodel
+                        }
+                    }
+                ]);
+
+                const optionsDE = {
+                    hostname: 'mcc62qyxnq0y3c33dwzj4mkhzw0q.rest.marketingcloudapis.com',
+                    path: '/hub/v1/dataevents/key:940A0128-1E5C-46AE-BE7E-DA7A8361D999/rowset',
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization' : 'Bearer '+authResponse.access_token
+                    }
+                };
+
+                console.log('optionsDE: '+JSON.stringify(optionsDE));
+                console.log('postDE: '+postDE);
+
+                const authDE = http.request(optionsDE, (resDE) => {
+                    let responseData = '';
+        
+                    resDE.on('data', (chunk) => {
+                        responseData += chunk;
+                    });
+        
+                    resDE.on('end', () => {
+                        let deResponse = JSON.parse(responseData);
+                        
+                        res.render("thanks", {} );
+                    });
+                });
+
+                authDE.on('error', (error) => {
+                    console.error(error);
+                });
+
+                authDE.write(postDE);
+                authDE.end();
+
+            });
         });
 
-        conn.version = '54.0';
+        authReq.on('error', (error) => {
+            console.error(error);
+        });
 
-        /*
-            IndicacaoNome__c : req.body.ownerName
-        */
-
-
-        
+        authReq.write(postData);
+        authReq.end();
 }
 
 module.exports =  {
